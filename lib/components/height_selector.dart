f5a648c3697fe61fb8b4c9131de2c340e929e0b2
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2025-04-02 12:45:03 -0300

    Update UI - task: state Slider

--- lib/components/height_selector.dart
+++ lib/components/height_selector.dart
@@ -3,7 +3,11 @@ import 'package:imc_calculator_app/core/app_colors.dart';
 import 'package:imc_calculator_app/core/text_styles.dart';
 
 class HeightSelector extends StatefulWidget {
-  const HeightSelector({super.key});
+  final double value;
+  final Function() onIncrement;
+  final Function() onDecrement;
+
+  const HeightSelector({super.key, required this.value, required this.onDecrement, required this.onIncrement});
 
   @override
   State<HeightSelector> createState() => _HeightSelectorState();
@@ -15,30 +19,39 @@ class _HeightSelectorState extends State<HeightSelector> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 16, top: 0, bottom: 16, right: 16),
+      padding: const EdgeInsets.only(left: 16, top: 0, bottom: 0, right: 16),
       child: Container(
         decoration: BoxDecoration(
-          color: AppColors.backgroundComponent,
-          borderRadius: BorderRadius.circular(16)
-        ),
+            color: AppColors.backgroundComponent,
+            borderRadius: BorderRadius.circular(16)),
         child: Column(
           children: [
-            SizedBox(height: 16,),
-            Text("ALTURA", style: TextStyles.bodyText,),
-            Text("${height.toStringAsFixed(0)} cm", style: TextStyle(color: Colors.white, fontSize: 38, fontWeight: FontWeight.bold),),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: Text(
+                "ALTURA",
+                style: TextStyles.bodyText,
+              ),
+            ),
+            Text(
+              "${height.toStringAsFixed(0)} cm",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 38,
+                  fontWeight: FontWeight.bold),
+            ),
             Slider(
-              value: height,
-              onChanged: (newHeight) {
-                setState(() {
-                  height = newHeight;
-                });
-              },
-              min: 140,
-              max: 220,
-              divisions: 80,
-              label: "${height.toStringAsFixed(0)} cm",
-              activeColor: AppColors.primary
-            )
+                value: height,
+                onChanged: (newHeight) {
+                  setState(() {
+                    height = newHeight;
+                  });
+                },
+                min: 140,
+                max: 220,
+                divisions: 80,
+                label: "${height.toStringAsFixed(0)} cm",
+                activeColor: AppColors.primary)
           ],
         ),
       ),
--- lib/screens/imc_home_screen.dart
+++ lib/screens/imc_home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:imc_calculator_app/components/gender_selector.dart';
 import 'package:imc_calculator_app/components/height_selector.dart';
+import 'package:imc_calculator_app/components/number_selector.dart';
+import 'package:imc_calculator_app/core/app_colors.dart';
+import 'package:imc_calculator_app/core/text_styles.dart';
 
 class ImcHomeScreen extends StatefulWidget {
   const ImcHomeScreen({super.key});
@@ -10,12 +13,72 @@ class ImcHomeScreen extends StatefulWidget {
 }
 
 class _ImcHomeScreenState extends State<ImcHomeScreen> {
+  double selectHeight = 170;
+  int selectAge = 20;
+  int selectedWeight = 80;
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         GenderSelector(),
-        HeightSelector()
+        HeightSelector(),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Expanded(
+                  child: NumberSelector(
+                title: "PESO",
+                value: selectedWeight,
+                onIncrement: () {
+                  setState(() {
+                    selectedWeight++;
+                  });
+                },
+                onDecrement: () {
+                  setState(() {
+                    selectedWeight--;
+                  });
+                },
+              )),
+              SizedBox(width: 16),
+              Expanded(
+                  child: NumberSelector(
+                title: "EDAD",
+                value: selectAge,
+                onIncrement: () {
+                  setState(() {
+                    selectAge++;
+                  });
+                },
+                onDecrement: () {
+                  setState(() {
+                    selectAge--;
+                  });
+                },
+              ))
+            ],
+          ),
+        ),
+        Spacer(),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SizedBox(
+            height: 60,
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                  shape: WidgetStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8))),
+                  backgroundColor: WidgetStateProperty.all(AppColors.primary)),
+              child: Text(
+                "Calcular",
+                style: TextStyles.bodyText,
+              ),
+            ),
+          ),
+        )
       ],
     );
   }
